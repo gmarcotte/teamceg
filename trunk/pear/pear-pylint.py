fcd1c9a5819a394c7a18ae74af2f8a4a5814fd12
garrett.marcotte <garrett.marcotte@a10243f8-fd84-11dd-a8e2-f7cb3c35fcff>
2009-03-11 04:00:51 +0000

    Adding wrapper and custom config for pylint.

---
+++ trunk/pear/pear-pylint.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+"""A thin wrapper around PyLint to conform to our style standards.
+Usage:
+  >> python pear-pylint.py file1 file2 ....
+"""
+
+__author__ = ['Garrett Marcotte (marcotte@princeton.edu)']
+
+# Python Modules
+import os
+import sys
+
+# Pear Modules
+import python_path
+
+# Pylint Modules
+from pylint import lint
+
+# Update the Python path so that PyLint knows about our vendor modules
+vendor_path = os.path.join(os.path.dirname(__file__), '../vendor')
+python_path.add_sys_path(vendor_path)
+
+opts = [
+    "--rcfile=pear-pylintrc.cfg",
+]
+opts.extend(sys.argv[1:])
+
+command = "pylint %s" % (' '.join(opts))
+print "Running: %s" % command
+
+lint.Run(opts)
