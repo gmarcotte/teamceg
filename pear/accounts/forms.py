b03e96f6b77af3819c26cbe90029eb95547beeba
ellenkim10 <ellenkim10@a10243f8-fd84-11dd-a8e2-f7cb3c35fcff>
2009-03-27 02:14:25 +0000

    RegistrationForm and LoginForm

--- pear/accounts/forms.py
+++ pear/accounts/forms.py
@@ -5,3 +5,10 @@ class RegistrationForm(forms.Form):
   email = forms.EmailField('E-Mail Address')
   first_name = forms.CharField('First Name', max_length=50, required=False)
   last_name = forms.CharField('Last Name', max_length=50, required=False)
+  class_year = forms.CharField('Class Year', max_length=4, required=False)
+  major = forms.CharField('Major', max_length=50, required=False)
+
+
+class LoginForm(forms.Form):
+  email = forms.EmailField('E-Mail Address')
+  password = forms.CharField('Password', max_length=50)
