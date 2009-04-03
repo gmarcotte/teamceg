eec828d8d4a709c591d97e1b08976dab88a34c51
garrett.marcotte <garrett.marcotte@a10243f8-fd84-11dd-a8e2-f7cb3c35fcff>
2009-04-03 21:25:00 +0000

    Adding timestamping to all models, and extending Django user via a proxy model.

--- pear/accounts/models.py
+++ pear/accounts/models.py
@@ -3,7 +3,40 @@
 from django.db import models
 from django.contrib.auth import models as auth_models
 
-class Profile(models.Model):
+from pear.core import timestamp
+
+class PearUser(auth_models.User):
+  """This model is used to extend the functionality of the Django user model
+  without changing the actual Django code.  We can only add methods and
+  attributes here, we can't add any new database fields.s
+  """
+  class Meta:
+    proxy = True
+    
+  def __unicode__(self):
+    return "%s: %s" % (self.email, self.get_full_name())
+  
+  def get_profile(self):
+    return self.profile.get()
+  
+  @property
+  def created_at(self):
+    return self.get_profile().created_at
+  
+  @property
+  def updated_at(self):
+    return self.get_profile().updated_at
+  
+  @property
+  def update_count(self):
+    return self.get_profile().update_count
+  
+  def save(self):
+    self.get_profile().save()
+    super(PearUser, self).save()
+
+
+class Profile(timestamp.TimestampedModel):
   major = models.CharField(max_length=30)
   class_year = models.CharField(max_length=30)
   user = models.ForeignKey(auth_models.User, related_name='profile')
