34c54b2ca36d53107f49f371b4592fa709e64bf1
garrett.marcotte <garrett.marcotte@a10243f8-fd84-11dd-a8e2-f7cb3c35fcff>
2009-04-05 03:53:45 +0000

    Adding a smarter if template tag

--- pear/core/templatetags/__init__.py
+++ pear/core/templatetags/__init__.py
@@ -3,3 +3,4 @@ from django import template
 # Make common template tags universally accessible
 template.add_to_builtins('pear.core.templatetags.forms')
 template.add_to_builtins('pear.core.templatetags.common')
+template.add_to_builtins('pear.core.templatetags.smart_if')
