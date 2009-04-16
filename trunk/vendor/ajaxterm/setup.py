23d5b41c06f176f16fb4103295c1441f4ca41c5e
cilvento <cilvento@a10243f8-fd84-11dd-a8e2-f7cb3c35fcff>
2009-04-16 03:51:56 +0000

    Adds the ajaxterm code to vendor.

---
+++ trunk/vendor/ajaxterm/setup.py
@@ -0,0 +1,17 @@
+#
+#  setup.py
+#  sandbox
+#
+#  Created by Christina Ilvento on 4/4/09.
+#
+from distutils.core import setup
+setup (name='ajaxterm',
+    version='0.10',
+    py_modules=['ajaxterm','qweb'],
+    description='',
+    author='',
+    author_email='',
+    url='',
+    license='',
+    platforms='UNIX',
+)
