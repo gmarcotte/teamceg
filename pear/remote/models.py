c1f14b42de6cbf21bd2bb6e04a853fc0acdd2c0f
garrett.marcotte <garrett.marcotte@a10243f8-fd84-11dd-a8e2-f7cb3c35fcff>
2009-05-07 22:02:05 +0000

    Changes to SSH to make remote connections work from server-side using pexpect.

--- pear/remote/models.py
+++ pear/remote/models.py
@@ -8,6 +8,12 @@ if settings.USE_PEXPECT:
   import pexpect
 import sys
 
+class AvailableSSHManager(models.Manager):
+  """A custom manager to only get SSH connections that have valid RSA keys installed."""
+  def get_query_set(self):
+    return super(
+        AvailableSSHManager, self).get_query_set().filter(has_valid_keys=True)
+
 
 class SSHConnection(timestamp.TimestampedModel):
   server = models.CharField(max_length=50)
@@ -15,6 +21,9 @@ class SSHConnection(timestamp.TimestampedModel):
   user = models.ForeignKey(pear.accounts.models.PearUser, related_name='servers')
   num_active = models.IntegerField()
   has_valid_keys = models.BooleanField()
+  base_dir = models.CharField(max_length=100)
+  
+  active_servers = AvailableSSHManager()
   
   def __unicode__(self):
     return '%s@%s' % (self.user_name, self.server)
@@ -38,43 +47,63 @@ class SSHConnection(timestamp.TimestampedModel):
       key_file = self.user.profile.get_private_file()
       login = "ssh -i "+ key_file + " " + self.user_name + "@" + self.server
       session = pexpect.spawn(login)
+      resp = self.read_output(session)
+      # If the SSH prompts for a password, then our connection failed
+      if resp.find("Password:") >= 0:
+        session.close()
+        return None
       self.num_active += 1
       self.save()
       return session
     else:
       return None
     
+  def read_output(self, session):
+    if settings.USE_PEXPECT:
+      txt = ''
+      while(True):
+        try:
+          txt += session.read_nonblocking(timeout=1)
+        except:
+          break
+      return txt
+    return ''
+
   def execute(self, session, command):
     """Execute a remote command over ssh, and return the response.
     Args:
       session: The pExpect object returned when initializing the connection.
       command: The command to run on the remote server
-    
+
     Returns:
       The output of running the command on the remote server.
     """
     if settings.USE_PEXPECT:
       session.sendline(command)
-      resp = session.read_nonblocking(size=2000, timeout=10)
-      return resp + session.read_nonblocking(size=2000, timeout=10)
+      resp = self.read_output(session)
+      lines = resp.split('\n')
+      resp = '\n'.join(lines[1:-1])
+      return resp
     else:
       return ''
-  
+    
   def send_control(self, session, control):
     """Send a control character to the remote server.
     Args:
       session: The pExpect object returned when initializing the connection.
       command: The control character to send.
-    
+
     Returns:
       The output of sending the control character to the remote server.
     """
     if settings.USE_PEXPECT:
       session.sendcontrol(control)
-      resp = session.read_nonblocking(size=2000, timeout=10)
-      return resp + session.read_nonblocking(size=2000, timeout=10)
-  
-  def close(session):
+      resp = self.read_output(session)
+      lines = resp.split('\n')
+      resp = '\n'.join(lines[1:-1])
+      return resp
+
+  def close(self, session):
     """Close an SSH session to a remote server.
     Args:
       session: The pExpect object returned when initializing the connection.
@@ -82,8 +111,7 @@ class SSHConnection(timestamp.TimestampedModel):
     session.sendline('exit')
     session.close()
     self.num_active -= 1
-    self.save()
-  
+    self.save()  
   
   ########## Authentication Methods #############
   def clear_remote_keys(self):
@@ -91,38 +119,34 @@ class SSHConnection(timestamp.TimestampedModel):
     longer log in to their account.
     """
     if settings.USE_PEXPECT:
-      cat = "cat " + self.user.profile.get_public_file()
-      listener = pexpect.spawn(cat)
-      pub_key = listener.read(800)
-      
       session = self.connect()
-      self.execute(session, 'cd .ssh')
-      self.execute(session, 'rm -f authorized_keys')
+      if session:
+        resp = self.execute(session, 'rm -f .ssh/authorized_keys')
+        self.close(session)
+        raise Exception(resp)
+
+    # See if clearing worked
+    session = self.connect()
+    if session:
+      self.has_valid_keys = True
+      self.save()
       self.close(session)
+      return False
+    else:
       self.has_valid_keys = False
       self.save()
       return True
-    return False
-  
+
   def set_remote_keys(self, password):
     """Sets the RSA key for the user on the server. The plaintext
     password must be given as an argument, raising a potential security risk.
     """
     if settings.USE_PEXPECT:
-      cat = "cat " + self.user.profile.get_public_file()
-      listener = pexpect.spawn(cat)
-      pub_key = listener.read(800)
-      login = "ssh " + self.username + "@" + self.server
+      pub_key = open(self.user.profile.get_public_file(), 'r').read().strip()
+      login = 'ssh %s "echo %s >> .ssh/authorized_keys"' % (self, pub_key)
       ssh = pexpect.spawn(login)
       ssh.expect(':')
+      ssh.sendline('yes')
+      ssh.expect(':')
       ssh.sendline(password)
-      ssh.sendline('cd .ssh')
-      ssh.sendline('rm -f authorized_keys')
-      ech = "echo '" + pub_key + "' > authorized_keys"
-      ssh.sendline(ech)
-      ssh.sendline('exit')
       ssh.close()
-      self.has_valid_keys = True
-      self.save()
-      return True
-    return False
