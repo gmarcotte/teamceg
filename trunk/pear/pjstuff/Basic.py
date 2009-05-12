44a21682b7c05935b4a8a4945e8da187c44a5477
cilvento <cilvento@a10243f8-fd84-11dd-a8e2-f7cb3c35fcff>
2009-05-12 01:07:59 +0000

    Attempt to fix bug where ajaxter steals keystrokes from new filename input box in add dialog.

--- trunk/pear/pjstuff/Basic.py
+++ trunk/pear/pjstuff/Basic.py
@@ -337,7 +337,7 @@ class Basic:
     elif request_info.method == 'get_file_tree':
       self.file_list = []
       for tpl in response:
-        #window.alert(str(tpl[0])+ " " + str(tpl[1]) + " " + str(tpl[2]))
+        window.alert(str(tpl[0])+ " " + str(tpl[1]) + " " + str(tpl[2]))
         self.file_list.append(tpl)
         
       
